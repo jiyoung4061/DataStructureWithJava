c073c6c5b01172441b3eb2941fde1faa8e78224e
jiyoung <jiyoung4061@naver.com>
2020-11-19 12:34:30 +0900

    2020.11.19 중간 commit

--- src/dataStructure_Stack/StackCalculator.java
+++ src/dataStructure_Stack/StackCalculator.java
@@ -11,64 +11,56 @@ public class StackCalculator {
 		Stack<Character> stack = new Stack<Character>();
 		char[] arr = infixExpression.toCharArray();
 		String res = "";
-		char top;
-
+		
 		for (int i = 0; i < arr.length; i++) {
-			switch (arr[i]) {
-			case '(': // 열린 괄호 => 무조건 push
+//			System.out.println(i+":"+res);
+			if(arr[i] == '(') {
 				stack.push(arr[i]);
-				break;
-			case ')': // 닫힌 괄호 => (이 나올때까지 pop해서 res에 붙여주기
+				
+			} else if(arr[i] == ')') {
 				while (stack.peek() != '(') {
 					res += stack.pop();
 				}
-				stack.pop(); // ( pop!!
-				break;
-			case '+':
-			case '-':
-//				if (stack.empty() || stack.peek() == '(') { // stack이 비어있거나, stack top이 (인 경우에는 무조건 push
-//					stack.push(arr[i]);
-//				} else { // 우선순위가 같거나 높은 연산자를 만났을 경우 => 연산자 모두..
-//					while (!stack.empty() && stack.peek() != '(') {
-//						res += stack.pop();
-//					}
-//					if (!stack.empty()) { // stack내에 요소가 남는경우 = ( 가 top에 있는경우
-//						stack.pop();
-//					}
-//					stack.push(arr[i]);
-//				}
-//				stack.push(arr[i]);
-//				break;
-				top = stack.peek();
-				if (top == '+' || top == '-'|| top == '/'|| top == '*') {  // 우선순위가 같거나 높은 연산자를 만났을 경우 => 연산자 모두..
-					while (!stack.empty() && top != '(') {
-						res += stack.pop();
-					}
-					if (top == '(') { // ( 가 top에 있는경우
-						stack.pop();
-					}
-				}
-				stack.push(arr[i]);
-				break;
-			case '*':
-			case '/':
-				// stack.peek()를 top으로 대체할수 없을가.....................................................................................................
-				if (stack.empty() || stack.peek() == '(' || stack.peek() == '+' || stack.peek() == '-') { // 바로 push해도 되는 경우
-					stack.push(arr[i]);
-				} else { // 우선 순위가 같은 경우
+				stack.pop(); // ( pop해주기
+				
+			} else if( arr[i] == '+' || arr[i] == '-' || arr[i] == '*' || arr[i]=='/') {				
+				if (!stack.empty() && (compareOfOperand(arr[i], stack.peek()) < 1) ) { //우선순위가 같거나 높은 연산자를 만났을 경우 => 연산자 모두..						
 					while (!stack.empty() && stack.peek() != '(') {
 						res += stack.pop();
 					}
-					if (stack.peek()=='(') { // stack내에 요소가 남는경우 = ( 가 top에 있는경우
-						stack.pop();
-					}
-					stack.push(arr[i]);
 				}
-				break;
-			default: // 숫자인 경우 무조건 res에
+				stack.push(arr[i]);
+				
+			} else {
 				res += arr[i];
-				break;
 			}
+			
+			
+//			switch (arr[i]) {
+//			case '(': // 열린 괄호 => 무조건 push
+//				stack.push(arr[i]);
+//				break;
+//			case ')': // 닫힌 괄호 => (이 나올때까지 pop해서 res에 붙여주기
+//				while (stack.peek() != '(') {
+//					res += stack.pop();
+//				}
+//				stack.pop(); // ( pop해주기
+//				break;
+//			case '+':
+//			case '-':
+//			case '/':
+//			case '*':
+//				if (!stack.empty() && (compareOfOperand(arr[i], stack.peek()) < 1) ) { //우선순위가 같거나 높은 연산자를 만났을 경우 => 연산자 모두..						
+//					while (!stack.empty() && stack.peek() != '(') {
+//						res += stack.pop();
+//					}
+//				}				
+//				stack.push(arr[i]);
+//				break;
+//			default: // 숫자인 경우 무조건 res에
+//				res += arr[i];
+//				break;
+//			}
 		}
 
 		while (!stack.empty()) { // stack에 남아있는 것들 다 pop
@@ -76,5 +68,43 @@ public class StackCalculator {
 		}
 		return res;
 	}
-
+	
+	public int compareOfOperand (char a, char b) {
+		// 연산자 a가 b보다 우선순위가 큰 경우 +1
+		// b가 더 큰경우 -1
+		// 같은 경우 == 0
+		int valOfReturn = 0;
+		switch(a) {
+		case '+':
+		case '-':
+			if( b == '/' || b == '*') valOfReturn = -1;
+			else valOfReturn = 0;
+			break;
+		case '*':
+		case '/':
+			if( b == '+' || b == '-') valOfReturn = 0;
+			else valOfReturn = 1;
+			break;
+		}
+		
+		return valOfReturn;
+	}
 }
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+// +,- 일 경우 if문
+// *,/ 일 경우 if문
+// while다음에
+//					if (stack.peek()=='(') { // stack내에 요소가 남는경우 = ( 가 top에 있는경우
+//						stack.pop();
+//					}
--- src/dataStructure_Stack/TestOfStackCalculator.java
+++ src/dataStructure_Stack/TestOfStackCalculator.java
@@ -6,8 +6,12 @@ public class TestOfStackCalculator {
 		// TODO Auto-generated method stub
 		
 		StackCalculator cal = new StackCalculator("(1+2)*(3/4)+(5+(6-7))");
+		StackCalculator cal2 = new StackCalculator("(1+2*3)/7");
+		StackCalculator cal3 = new StackCalculator("2*3/(2+4)+1");
 		// 12+34/*567-++
 		System.out.println(cal.infixToPostfix());
+//		System.out.println(cal2.infixToPostfix());
+//		System.out.println(cal3.infixToPostfix());
 	}
 
 }
