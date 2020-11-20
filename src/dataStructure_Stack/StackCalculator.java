cb3941d78e8ce12227382183f83e2461ad4458cf
jiyoung <jiyoung4061@naver.com>
2020-11-20 10:21:24 +0900

    2020.11.20 stack을 사용한 계산기

--- src/dataStructure_Stack/StackCalculator.java
+++ src/dataStructure_Stack/StackCalculator.java
@@ -2,6 +2,7 @@ package dataStructure_Stack;
 
 public class StackCalculator {
 	private String infixExpression;
+	String res = "";
 
 	public StackCalculator(String infixExpression) {
 		this.infixExpression = infixExpression;
@@ -10,26 +11,35 @@ public class StackCalculator {
 	public String infixToPostfix() {
 		Stack<Character> stack = new Stack<Character>();
 		char[] arr = infixExpression.toCharArray();
-		String res = "";
-		
+
 		for (int i = 0; i < arr.length; i++) {
-			if(arr[i] == '(') {
+			if (arr[i] == '(') {
 				stack.push(arr[i]);
-			} else if(arr[i] == ')') {
+			} else if (arr[i] == ')') {
 				while (stack.peek() != '(') {
 					res += stack.pop();
 				}
-				stack.pop(); 
-			} else if( arr[i] == '+' || arr[i] == '-' || arr[i] == '*' || arr[i]=='/') {
-				if (!stack.empty() && (compareOfOperand(arr[i], stack.peek()) < 1) ) {
+				stack.pop();
+			} else if (arr[i] >= '0' && arr[i] <= '9') {
+				res += arr[i];
+			} else {
+				if (!stack.empty() && (compareOfOperand(arr[i], stack.peek()) < 1)) {
 					while (!stack.empty() && stack.peek() != '(') {
 						res += stack.pop();
 					}
 				}
 				stack.push(arr[i]);
-			} else {
-				res += arr[i];
 			}
+//			else if( arr[i] == '+' || arr[i] == '-' || arr[i] == '*' || arr[i]=='/') {
+//				if (!stack.empty() && (compareOfOperand(arr[i], stack.peek()) < 1) ) {
+//					while (!stack.empty() && stack.peek() != '(') {
+//						res += stack.pop();
+//					}
+//				}
+//				stack.push(arr[i]);
+//			} else { 
+//				res += arr[i];
+//			}
 		}
 		while (!stack.empty()) { // stack에 남아있는 것들 다 pop
 			res += stack.pop();
@@ -37,23 +47,107 @@ public class StackCalculator {
 		return res;
 	}
 
-	public int compareOfOperand (char a, char b) {
+	public int compareOfOperand(char a, char b) {
 		// 연산자 a가 b보다 우선순위가 큰 경우 +1
 		// b가 더 큰경우 -1
 		// 같은 경우 == 0
-		int valOfReturn = 0;
-		switch(a) {
+//		int valOfReturn = 0;
+//		switch(a) {
+//		case '+':
+//		case '-':
+//			if( b == '/' || b == '*') valOfReturn = -1;
+//			else valOfReturn = 0;
+//			break;
+//		case '*':
+//		case '/':
+//			if( b == '+' || b == '-') valOfReturn = 0;
+//			else valOfReturn = 1;
+//			break;
+//		}
+//		return valOfReturn;
+
+		// 선생님 방법
+		if (a == '*' || a == '/') {
+			if (b == '/' || b == '*') {
+				return 0;
+			} else {
+				return 1;
+			}
+		} else {
+			if (b == '/' || b == '*') {
+				return -1;
+			} else {
+				return 0;
+			}
+		}
+	}
+
+//	//첫번째 풀이 : return 값이 char
+//	public char CalculatorUsingStack() {
+//		
+//		char[] arrayOfRes = res.toCharArray();
+//		Stack<Character> stack = new Stack<Character>();
+//		
+//		for(int i = 0 ; i < arrayOfRes.length ; i++) {		
+//			if(arrayOfRes[i] > '0' && arrayOfRes[i] <= '9') { // 숫자일 경우
+//				stack.push(arrayOfRes[i]);
+//			} else { // 연산자일경우
+//				int a = stack.pop();
+//				int b = stack.pop();
+//				stack.push((char) getResult(b, a, arrayOfRes[i]));
+//			}
+//		}
+//		return stack.pop();
+//	}
+
+//	// 두번째 풀이 : return값을 int값으로 해주기위해 stack을 Integer 타입으로 받음 => char배열이라 int로 바꾸는 빈번함
+//	public int CalculatorUsingStack() {
+//		
+//		char[] arrayOfRes = res.toCharArray();
+//		Stack<Integer> stack = new Stack<Integer>();
+//		
+//		for(int i = 0 ; i < arrayOfRes.length ; i++) {		
+//			if(arrayOfRes[i]-'0' > 0 && arrayOfRes[i]-'0' <= 9) { // 숫자일 경우
+//				stack.push(arrayOfRes[i]-'0');
+//			} else { // 연산자일경우
+//				stack.push(getResult(stack.pop(), stack.pop(), arrayOfRes[i]));
+//			}
+//		}
+//		return stack.pop();
+//	}
+
+//	// 최종 풀이 : char로 값을 계산하되 마지막 return에서 char를 int로 바꿔줌
+	public int CalculatorUsingStack() {
+		
+		char[] arrayOfRes = res.toCharArray();
+		Stack<Character> stack = new Stack<Character>();
+		
+		for (int i = 0; i < arrayOfRes.length; i++) {
+			if (arrayOfRes[i] > '0' && arrayOfRes[i] <= '9') { // 숫자일 경우
+				stack.push(arrayOfRes[i]);
+			} else { // 연산자일경우
+				stack.push((char) getResult(stack.pop(), stack.pop(), arrayOfRes[i]));
+			}
+		}
+		return stack.pop() - '0';
+	}
+	
+	public int getResult(int a, int b, char c) {
+		int result = 0;
+		switch (c) {
 		case '+':
+			result = b + a;
+			break;
 		case '-':
-			if( b == '/' || b == '*') valOfReturn = -1;
-			else valOfReturn = 0;
+			result = b - a;
 			break;
 		case '*':
+			result = b * a;
+			break;
 		case '/':
-			if( b == '+' || b == '-') valOfReturn = 0;
-			else valOfReturn = 1;
+			result = b / a;
 			break;
 		}
-		return valOfReturn;
+		return result;
 	}
 }
--- src/dataStructure_Stack/TestOfStackCalculator.java
+++ src/dataStructure_Stack/TestOfStackCalculator.java
@@ -8,10 +8,12 @@ public class TestOfStackCalculator {
 		StackCalculator cal = new StackCalculator("(1+2)*(3/4)+(5+(6-7))");
 		StackCalculator cal2 = new StackCalculator("(1+2*3)/7");
 		StackCalculator cal3 = new StackCalculator("2*3/(2+4)+1");
+		StackCalculator cal4 = new StackCalculator("2+3*4");
 
 		System.out.println(cal.infixToPostfix());
-		System.out.println(cal2.infixToPostfix());
-		System.out.println(cal3.infixToPostfix());
+//		System.out.println(cal2.infixToPostfix());
+//		System.out.println(cal3.infixToPostfix());
+//		System.out.println(cal4.infixToPostfix());
+		System.out.println(cal.CalculatorUsingStack());
 	}
-
 }
